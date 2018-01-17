e664742ba02498f564c0bdfc5d5a715141e54e5c
Jeff Levensailor <jlevensailor@presidio.com>
2018-01-16 20:40:15 -0800

    updated package and readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # accounts-ciscospark
 
 A login service for Cisco Spark.
+
+To use: **meteor add levensailor:accounts-ciscospark**
--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
+  name: "levensailor:accounts-ciscospark",
   summary: "Login service for Ciscospark accounts",
   version: "1.0.0",
   git: "https://github.com/levensailor/accounts-ciscospark.git"
 });
 
 Package.onUse(function(api) {
-  api.use(['underscore', 'random']);
-  api.use('accounts-base', ['client', 'server']);
+  api.use(['underscore@1.0.9', 'random@1.0.10']);
+  api.use('accounts-base@1.0.4', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('ciscospark', ['client', 'server']);
+  api.imply('accounts-base@1.0.4', ['client', 'server']);
+  api.use('accounts-oauth@1.1.13', ['client', 'server']);
+  api.use('levensailor:ciscospark@1.0.0', ['client', 'server']);
 
   api.addFiles('ciscospark_login_button.css', 'client');
 
